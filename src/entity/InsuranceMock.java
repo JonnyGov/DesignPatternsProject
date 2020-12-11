1f8123db6a171e8099f3933cce44c3e1daf9dde2
Ofek Shachar <48984874+OfekSha@users.noreply.github.com>
2020-12-12 00:12:50 +0200

    fix insurance Mock

--- src/entity/InsuranceMock.java
+++ src/entity/InsuranceMock.java
@@ -3,7 +3,8 @@ package entity;
 import java.util.Date;
 
 public class InsuranceMock implements InsuranceData {
-
+	String name="poop",family="hoop",type="goochi",remarks="good";
+	Date date=new Date(System.currentTimeMillis());
     @Override
     public String getName() {
         return "name";
